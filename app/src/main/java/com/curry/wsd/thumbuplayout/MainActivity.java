990106b04be98c0f1420ac97c0750878a0b297cc
wsd7747 <774738009@qq.com>
2017-09-22 10:33:06 +0800

    完整项目提交

--- app/src/main/java/com/curry/wsd/thumbuplayout/MainActivity.java
+++ app/src/main/java/com/curry/wsd/thumbuplayout/MainActivity.java
@@ -2,12 +2,27 @@ package com.curry.wsd.thumbuplayout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.curry.wsd.thumbuplayout.view.ThumbUpLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ThumbUpLayout thumbUpLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        thumbUpLayout= (ThumbUpLayout) findViewById(R.id.thumbUpLayout);
+    }
+
+    public void OnClick(View v){
+        switch (v.getId()){
+            case R.id.iv:
+                thumbUpLayout.addHeart();
+
+                break;
+        }
     }
 }
